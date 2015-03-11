dd911257971e861b64dfc3230349833059cada76
Martin A Segado <msegado@mit.edu>
2015-03-11 00:57:47 -0400

    A mixin helper; needs tests

--- sketch_tool/lib/main.js
+++ sketch_tool/lib/main.js
@@ -1,4 +1,15 @@
+import { baseWithMixins } from './util';
+import events from 'events';
+let EventEmitter = events.EventEmitter;
+
 console.log('Hello from main.js');
+
+
+class Foo extends baseWithMixins(Object, EventEmitter) { }
+
+window.f = new Foo();
+console.log(f)
+
 
 export const x = 42;
 
--- sketch_tool/lib/util.js
+++ sketch_tool/lib/util.js
@@ -15,3 +15,16 @@ export function observable(obj) {
     obj.emit = emitter.emit.bind(emitter);
     obj.once = emitter.once.bind(emitter);
 }
+
+/*
+Mixin helper. Sets up a class (intended for use with 'extends') whose prototype
+mixes the enumerable/own properties of the arguments' prototypes. Properties are
+updated left-to-right, so the right-most class methods have highest priority.
+*/
+export function baseWithMixins(Base, ...mixins) {
+    class NewBase extends Base {}
+    for (let Mixin of mixins) {
+        Object.assign(NewBase.prototype, Mixin.prototype);
+    }
+    return NewBase;
+}
