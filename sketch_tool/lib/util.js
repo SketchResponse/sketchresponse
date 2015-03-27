4659b71f0b9b6fe355d8152e5adc2964b84780a2
Martin A Segado <msegado@mit.edu>
2015-03-27 13:37:32 -0400

    Remove custom observable implementation for now

--- sketch_tool/lib/util.js
+++ sketch_tool/lib/util.js
@@ -1,21 +1,3 @@
-// TODO: change this to `import { EventEmitter } from 'events'` once that is
-// supported by SystemJS (see https://github.com/systemjs/systemjs/issues/334)
-import events from 'events';
-let EventEmitter = events.EventEmitter;
-
-export function observable(obj) {
-    // Check for __eventEmitter first to avoid overwriting it
-    if (obj.hasOwnProperty('__eventEmitter')) return;
-
-    let emitter = obj.__eventEmitter = new EventEmitter();
-
-    // Expose a subset of the EventEmitter interface on the object itself
-    obj.on = emitter.on.bind(emitter);
-    obj.off = emitter.removeListener.bind(emitter);
-    obj.emit = emitter.emit.bind(emitter);
-    obj.once = emitter.once.bind(emitter);
-}
-
 /*
 Mixin helper. Sets up a class (intended for use with 'extends') whose prototype
 mixes the enumerable/own properties of the arguments' prototypes. Properties are
