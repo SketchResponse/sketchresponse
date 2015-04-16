fbeb8810e093bae6a04955ddb56606d8dff0ae21
Martin A Segado <msegado@mit.edu>
2015-04-16 15:21:19 -0400

    Better SI class tests

--- sketch_tool/test/main.spec.js
+++ sketch_tool/test/main.spec.js
@@ -1,17 +1,19 @@
 import SketchInput from 'sketch2/main';
 
 describe('The SketchInput class', () => {
+  let el;
+
+  beforeEach(() => {
+    el = document.createElement('div');
+  });
 
   it('should be instantiated with a target element', () => {
-    const el = document.createElement('div');
     const si = new SketchInput(el);
-    expect(si.el).toBe(el);
+    expect(si instanceof SketchInput).toBe(true);
   });
 
   it('should create elements for the toolbar and sketch container', () => {
-    const el = document.createElement('div');
     const si = new SketchInput(el);
-
     expect(el.children[0].id).toEqual('si-toolbar');
     expect(el.children[1].id).toEqual('si-canvas');
   });
