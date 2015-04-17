02b00f9f8f3626af96d3cdbe2561d56dfe03850a
Martin A Segado <msegado@mit.edu>
2015-04-17 14:22:03 -0400

    Refactor toolbar tests

--- sketch_tool/test/toolbar.spec.js
+++ sketch_tool/test/toolbar.spec.js
@@ -14,10 +14,9 @@ describe('The toolbar', () => {
   });
 
   it('renders a single button item', () => {
-    const items = [
+    const tb = new Toolbar(el, app, [
       {type: 'button', id: 'testID', icon: 'data:test', label: 'Test label'}
-    ];
-    const tb = new Toolbar(el, app, items);
+    ]);
     expect(el.children.length).toBe(1);
     expect(el.children[0].nodeName.toLowerCase()).toEqual('li');
     expect(el.querySelector('button').id).toEqual('testID')
@@ -26,28 +25,27 @@ describe('The toolbar', () => {
   });
 
   it('renders a separator', () => {
-    const items = [
+    const tb = new Toolbar(el, app, [
       {type: 'separator'}
-    ];
-    const tb = new Toolbar(el, app, items);
+    ]);
     expect(el.querySelectorAll('li > hr').length).toEqual(1)
   });
 
   it('renders a splitbutton with an indicator after the label and a nested menu', () => {
-    const items = [
+    const tb = new Toolbar(el, app, [
       {type: 'splitbutton', id: 'testID', label: "Test label", items: [
+        {id:'subID0', icon: 'data:test0'},
         {id:'subID1', icon: 'data:test1'},
         {id:'subID2', icon: 'data:test2'},
-        {id:'subID3', icon: 'data:test3'},
       ]}
-    ];
-    const tb = new Toolbar(el, app, items);
+    ]);
 
     expect(el.querySelector('.tb-label').innerHTML).toMatch(/Test label\S+/);
     expect(el.querySelector('.tb-dropdown').children.length).toEqual(3);
 
-    for (let item of items[0].items) {
-      expect(el.querySelector(`#${item.id} img`).src).toContain(item.icon);
+    for (let idx of [0, 1, 2]) {
+      expect(el.querySelectorAll('.tb-dropdown-button')[idx].id).toEqual(`subID${idx}`);
+      expect(el.querySelectorAll('.tb-dropdown-icon')[idx].src).toEqual(`data:test${idx}`);
     }
   });
 });
