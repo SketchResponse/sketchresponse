5e8c2f2b3ae77fd87ad743377220c3bc84946307
Martin A Segado <msegado@mit.edu>
2015-05-01 16:30:53 -0400

    Add better state init (UNTESTED)

--- sketch_tool/lib/toolbar.js
+++ sketch_tool/lib/toolbar.js
@@ -1,6 +1,10 @@
 // from http://stackoverflow.com/a/5775621/1974654
 const NULL_SRC = '//:0';
 
+function isSplitButton(item) {
+  return item.type === 'splitbutton';
+}
+
 export default class Toolbar {
   constructor(el, app, items) {
     this.el = el;
@@ -8,11 +12,7 @@ export default class Toolbar {
     this.items = items;
 
     this.isActive = false;
-    this.state = {
-      openDropdownID: null,
-      activeItemID: null,
-      focusedItemID: null,
-    };
+    this.state = this.getInitialState();
 
     app.on('tb-dropdown-open', id => {
       this.state.openDropdownID = id;
@@ -20,6 +20,23 @@ export default class Toolbar {
     });
 
     this.render({firstTime: true});
+  }
+
+  getInitialState() {
+    const firstItem = this.items[0];
+
+    let state = {
+      openDropdownID: null,
+      activeItemID: isSplitButton(firstItem) ? firstItem.items[0].id : firstItem.id,
+      focusedItemID: firstItem.id,
+      selectedDropdownItemMap: {},
+    };
+
+    this.items
+      .filter(isSplitButton)
+      .forEach(item => state.selectedDropdownItemMap[item.id] = item.items[0].id);
+
+    return state;
   }
 
   render({firstTime}) {
