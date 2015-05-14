88f3fa8f9708e380624f84bb0aa83955e6483a41
Martin A Segado <msegado@mit.edu>
2015-05-14 14:18:24 -0400

    Render toolbar into page

--- sketch_tool/lib/main.js
+++ sketch_tool/lib/main.js
@@ -1,11 +1,14 @@
+import Toolbar from './toolbar';
 
 export default class SketchInput {
-  constructor(el) {
+  constructor(el, toolbarItems) {
     this.el = el;
 
     this.el.innerHTML = `
       <menu id="si-toolbar"></menu>
       <svg id="si-canvas"></svg>
     `;
+
+    new Toolbar(document.getElementById('si-toolbar'), this, toolbarItems);
   }
 }
