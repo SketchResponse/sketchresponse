38513632a7b0ae267021dc59461fdf23fd76507e
Martin Segado <msegado@mit.edu>
2015-06-27 18:48:42 -0400

    Remove old toolbar + tests for now

--- sketch_tool/lib/main.js
+++ sketch_tool/lib/main.js
@@ -1,8 +1,7 @@
 import './polyfills';
-import Toolbar from './toolbar';
 
 export default class SketchInput {
-  constructor(el, toolbarItems) {
+  constructor(el) {
     if (!(el instanceof HTMLElement)) throw new TypeError(
       'The first argument to the SketchInput constructor must be an HTMLElement'
     );
@@ -13,14 +12,5 @@ export default class SketchInput {
       <menu id="si-toolbar"></menu>
       <svg id="si-canvas" touch-action="none"></svg>
     `;
-
-    if (toolbarItems) {
-      this.toolbar = new Toolbar(document.getElementById('si-toolbar'), this, toolbarItems);
-    }
-  }
-
-  dispatch(type, id, subId) {
-    // TODO: don't hard-code this... perhaps refactor toolbar events to be internal?
-    this.toolbar.handleEvent({type, id, subId});
   }
 }
