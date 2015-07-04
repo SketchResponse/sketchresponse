469ca0d55fcebfd4f4ef750f4066f6b98a99430f
Martin Segado <msegado@mit.edu>
2015-07-04 14:16:07 -0400

    Upgrade jspm to 0.16 beta (which uses newer SystemJS) and several other packages to latest. Note that importing babel/polyfill broke; switched to core-js/shim

--- sketch_tool/config.js
+++ sketch_tool/config.js
@@ -1,4 +1,5 @@
 System.config({
+  "defaultJSExtensions": true,
   "transpiler": "babel",
   "babelOptions": {
     "optional": [
@@ -6,19 +7,18 @@ System.config({
     ]
   },
   "paths": {
-    "*": "*.js",
-    "sketch2/*": "lib/*.js",
-    "github:*": "jspm_packages/github/*.js",
-    "npm:*": "jspm_packages/npm/*.js"
+    "sketch2/*": "lib/*",
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
   }
 });
 
 System.config({
   "map": {
-    "babel": "npm:babel-core@5.2.17",
-    "babel-runtime": "npm:babel-runtime@5.2.17",
+    "babel": "npm:babel-core@5.6.15",
+    "babel-runtime": "npm:babel-runtime@5.6.15",
     "classnames": "npm:classnames@2.1.1",
-    "core-js": "npm:core-js@0.9.7",
+    "core-js": "npm:core-js@0.9.18",
     "d3": "github:mbostock/d3@3.5.5",
     "dom-shims": "npm:dom-shims@0.1.4",
     "events": "github:jspm/nodelibs-events@0.1.1",
@@ -40,13 +40,18 @@ System.config({
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
+    "npm:babel-runtime@5.6.15": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
     "npm:classnames@2.1.1": {
       "assert": "github:jspm/nodelibs-assert@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
-    "npm:core-js@0.9.7": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
+    "npm:core-js@0.9.18": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
