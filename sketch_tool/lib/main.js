9d4d379972c643a4956f5bbcfb57233dabae07d1
Martin Segado <msegado@mit.edu>
2015-07-26 22:40:54 -0400

    Factor out inheriting object tree functionality

--- sketch_tool/lib/main.js
+++ sketch_tool/lib/main.js
@@ -1,4 +1,5 @@
 import './util/polyfills';
+import { createInheritingObjectTree } from './util/inheriting-object-tree';
 
 import { EventEmitter } from 'events';
 import Mousetrap from 'mousetrap';
@@ -10,26 +11,6 @@ import HistoryManager from './history-manager';
 
 import Toolbar from './toolbar';
 
-function createInheritingObjectTree(oldObj, parent=Object.prototype) {
-  const typeString = Object.prototype.toString.call(oldObj);
-
-  if (typeString === '[object Object]') {
-    // oldObj is a plain(ish) javascript object; make an inheriting variant and recurse
-    const newObj = Object.create(parent);
-    Object.keys(oldObj).forEach(key => {
-      newObj[key] = createInheritingObjectTree(oldObj[key], newObj);
-    });
-    return newObj;
-  }
-  else if (typeString === '[object Array]') {
-    // oldObj is an array; map it to an array with items inheriting from last known parent object
-    return oldObj.map(item => createInheritingObjectTree(item, parent));
-  }
-  else {
-    // oldObj is something else (primitive, Map, etc.); copy it without any inheritance
-    return oldObj;
-  }
-}
 
 export default class SketchInput {
   constructor(el, config) {
