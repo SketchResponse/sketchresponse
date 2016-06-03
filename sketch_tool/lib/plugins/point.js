85158eb529a9fce67ac768d8100b72db295dd11d
Jean-Michel Claus <jmc@math.mit.edu>
2016-06-03 16:36:03 +0200

    Add a 'base-plugin' from which all plugins that draw on the SVG canvas will inherit in the future. For the time being, extend the 'point' plugin from it. Add x and y bounds checkers that will freeze a group drag in a direction if one of its member has reached the boundaries.

--- sketch_tool/lib/plugins/point.js
+++ sketch_tool/lib/plugins/point.js
@@ -1,52 +1,17 @@
 import z from 'sketch2/util/zdom';
+import BasePlugin from './base-plugin';
 
 export const VERSION = '0.1';
 export const GRADEABLE_VERSION = '0.1';
 
-// TODO: move some of these into 'params.defaults'?
-const ROUNDING_PRESCALER = 100;  // e.g., Math.round(value * ROUNDING_PRESCALER) / ROUNDING_PRESCALER
-
-export default class Point {
+export default class Point extends BasePlugin {
   constructor(params, app) {
-    this.params = params;
-    this.app = app;
-
-    this.el = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-    app.svg.appendChild(this.el);
-
-    this.state = [];
-
-    this.bindEventHandlers();
-
-    app.registerState({
-      id: params.id,
-      dataVersion: VERSION,
-      getState: () => this.state,
-      setState: state => { this.state = state; this.render(); },
-    });
-
-    app.registerGradeable({
-      id: params.id,
-      version: GRADEABLE_VERSION,
-      getGradeable: () => this.getGradeable(),
-    });
-
-    app.registerToolbarItem({
-      type: 'button',
-      id: params.id,
-      label: params.label,
-      icon: {
-        src: './plugins/point/GOOGLE_ic_gps_fixed_24px.svg',
-        alt: 'Point tool',
-      },
-      activate: this.activate.bind(this),
-      deactivate: this.deactivate.bind(this),
-    });
-  }
-
-  bindEventHandlers() {
-    ['drawStart', 'drawMove', 'drawEnd']
-      .forEach(name => this[name] = this[name].bind(this));
+    // Add params that are specific to this plugin
+    params.icon = {
+      src: './plugins/point/GOOGLE_ic_gps_fixed_24px.svg',
+      alt: 'Point tool'
+    };
+    super(params, app);
   }
 
   getGradeable() {
@@ -57,54 +22,14 @@ export default class Point {
     });
   }
 
-  activate() {
-    this.app.svg.addEventListener('pointerdown', this.drawStart);
-    this.app.svg.style.cursor = 'crosshair';
-  }
-
-  deactivate() {
-    this.app.svg.removeEventListener('pointerdown', this.drawStart);
-    this.app.svg.style.cursor = null;
-  }
-
-  drawStart(event) {
-    // this.app.svg.setPointerCapture(event.pointerId);
-    // this.app.svg.addEventListener('pointermove', this.drawMove);
-    this.app.svg.addEventListener('pointerup', this.drawEnd);
-    this.app.svg.addEventListener('pointercancel', this.drawEnd);
-    // this.drawMove(event);
-
+  // This will be called when clicking on the SVG canvas after having
+  // selected the point shape
+  initDraw(event) {
     this.currentPosition = {
-      x: clamp(event.clientX - this.params.left, 0, this.params.width),
-      y: clamp(event.clientY - this.params.top, 0, this.params.height),
+      x: event.clientX - this.params.left,
+      y: event.clientY - this.params.top,
     };
     this.state.push(this.currentPosition);
-
-    this.render();  //.then(refs => {
-    //   app.registerElement({
-    //     element: refs['tentative'],
-    //     initialBehavior: 'drag',
-    //   })
-    // })
-  }
-
-  drawMove(event) {
-    this.currentPosition = {
-      x: clamp(event.clientX - this.params.left, 0, this.params.width),
-      y: clamp(event.clientY - this.params.top, 0, this.params.height),
-    };
-    this.render();
-  }
-
-  // TODO: this adds state event when pointer was cancelled. add a drawCancel method?
-  drawEnd(event) {
-    // this.app.svg.releasePointerCapture(event.pointerId);
-    // this.app.svg.removeEventListener('pointermove', this.drawMove);
-    this.app.svg.removeEventListener('pointerup', this.drawEnd);
-    this.app.svg.removeEventListener('pointercancel', this.drawEnd);
-    // this.state.push(this.currentPosition);
-    this.currentPosition = undefined;
-    // this.app.addUndoPoint();
     this.render();
   }
 
@@ -125,33 +50,29 @@ export default class Point {
               element: el,
               initialBehavior: 'drag',
               onDrag: ({dx, dy}) => {
-                position.x += dx;   /// BUG: TODO!!!!: the position object changes when we undo/redo...
+                /// BUG: TODO!!!!: the position object changes when we undo/redo...
+                position.x += dx;
                 position.y += dy;
                 this.render();
+              },
+              inBoundsX: (dx) => {
+                return this.inBoundsX(position.x + dx);
+              },
+              inBoundsY: (dy) => {
+                return this.inBoundsY(position.y + dy)
               },
             });
           }
         })
       )
-      // // TODO: eliminate code duplication
-      // z.if(this.currentPosition !== undefined, () =>
-      //   z('circle', {
-      //     cx: this.currentPosition.x,
-      //     cy: this.currentPosition.y,
-      //     r: this.params.size / 2,
-      //     style: `
-      //       fill: ${this.params.color};
-      //       stroke-width: 0;
-      //       opacity: 0.7;
-      //     `,
-      //   })
-      // )
     );
   }
-}
 
-function clamp(value, min, max) {
-  if (value < min) return min;
-  if (value > max) return max;
-  return value;
+  inBoundsX(x) {
+    return x >= this.bounds.xmin && x <= this.bounds.xmax;
+  }
+
+  inBoundsY(y) {
+    return y >= this.bounds.ymin && y <= this.bounds.ymax;
+  }
 }
