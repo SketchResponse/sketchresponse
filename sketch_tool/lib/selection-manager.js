04331dc97ccaaefa98784303f304ab451e2294c9
Jean-Michel Claus <jmc@math.mit.edu>
2016-06-03 10:35:42 +0200

    Add msegado's work from branch 'msegado-element-manager' situated on MIT's github.

---
+++ sketch_tool/lib/selection-manager.js
@@ -0,0 +1,44 @@
+import * as attrCache from './util/dom-attr-cache';
+import { injectStyleSheet, injectSVGDefs } from './util/dom-style-helpers';
+
+// Note: data-* attributes are not officially supported in SVG 1.1 (without namespaces),
+// but they still seem to have widespread browser support and are a bit cleaner to deal with
+// if we ever have to tag both HTML and SVG elements
+export const SELECTED_ATTR = 'data-si-selected';
+
+// Inject stylesheet and filter on module load since we only need one each per window
+injectStyleSheet(`[${ SELECTED_ATTR }] { filter: url(#si-selected-filter); }`);
+injectSVGDefs(`
+  <filter id="si-selected-filter" x="-2" y="-2" width="5" height="5">
+    <feMorphology in="SourceAlpha" operator="dilate" radius="5"/>
+    <feColorMatrix type="matrix" values="
+      0 0 0 0 0
+      0 0 0 0 0
+      0 0 0 0 0
+      0 0 0 0.15 0
+    " />
+    <feComposite in="SourceGraphic" />
+  </filter>
+`);
+
+export default class SelectionManager {
+  constructor(rootElement) {
+    this.rootElement = rootElement;
+  }
+
+  select(element) { attrCache.setAttributeNS(element, null, SELECTED_ATTR, ''); }
+  deselect(element) { attrCache.removeAttributeNS(element, null, SELECTED_ATTR); }
+  isSelected(element) {return attrCache.getAttributeNS(element, null, SELECTED_ATTR) !== null; }
+
+  toggleSelected(element, condition) {
+    if (arguments.length < 2) condition = !this.isSelected(element);  // toggle current value
+    if (condition) this.select(element);
+    else this.deselect(element);
+  }
+
+  // Expensive; call circumspectly
+  getSelected() { return Array.from(this.rootElement.querySelectorAll(`[${ SELECTED_ATTR }]`)); }
+
+  // Expensive; call circumspectly
+  deselectAll() { this.getSelected().forEach(element => this.deselect(element)); }
+}
---
+++ sketch_tool/lib/util/fastattr.js
@@ -0,0 +1,50 @@
+const CACHE = '$__FastAttr_Cache';
+const NONE = '$__FastAttr_None';
+
+class FastAttr {
+  constructor(el) {
+    this.el = el;
+    this.cacheMap = el[CACHE] || (el[CACHE] = {});
+  }
+
+  getAttributeNS(ns, name) {
+    const cache = this.getCache(ns, name);
+    return cache.value === NONE ? undefined : cache.value;
+  }
+
+  setAttributeNS(ns, name, value) {
+    const cache = this.getCache(ns, name);
+    if (cache.value === value) return;
+    cache.ns = ns;
+    cache.name = name;
+    cache.value = value;
+    this.schedule(cache);
+  }
+
+  removeAttributeNS(ns, name) { this.setAttributeNS(ns, name, NONE); }
+
+  getAttribute(name, value) { this.getAttributeNS(null, name); }
+  setAttribute(name, value) { this.setAttributeNS(null, name, value); }
+  removeAttribute(name, value) { this.setAttributeNS(null, name, NONE); }
+
+  getCache(ns, name) {
+    const key = (ns || NONE) + '|' + name;
+    return this.cacheMap[key] || (this.cacheMap[key] = {});
+  }
+
+  schedule(cache) {
+    if (cache.pending) return;
+    window.requestAnimationFrame(this.flush.bind(this));
+    cache.pending = true;
+  }
+
+  flush() {
+    for (let key in this.cacheMap) {  // Assume only own properties
+      const cache = this.cacheMap[key];
+      if (!cache.pending) continue;
+      if (cache.value === NONE) this.el.removeAttributeNS(cache.ns, cache.name);
+      else this.el.setAttributeNS(cache.ns, cache.name, cache.value);
+      cache.pending = false;
+    }
+  }
+}
