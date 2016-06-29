75f1b5c3c3a1ca5b7b50cc6e9dd37ddbdf5e1742
Jean-Michel Claus <jmc@math.mit.edu>
2016-06-29 13:20:54 +0200

    New 'line-segment' plugin.

--- sketch_tool/html/debugConfigs.js
+++ sketch_tool/html/debugConfigs.js
@@ -28,7 +28,22 @@ function loadConfig(configId) {
         {name: 'freeform', id: 'f', label: 'Function f(x)', color: 'blue'},
         {name: 'freeform', id: 'g', label: 'Derivative g(x)', color: 'orange'},
       ]
-    }
+    },
+    config3: {
+      width: 750,
+      height: 420,
+      xrange: [-4.5, 4.5],
+      yrange: [-2.5, 2.5],
+      xscale: 'linear',
+      yscale: 'linear',
+      plugins: [
+        {name: 'axes'},
+        {name: 'point', id: 'cp', label: 'Point', color: 'dimgray', size: 15},
+        {name: 'horizontal-line', id: 'ha', label: 'Horizontal line', color: 'dimgray', dashStyle: 'dashdotted'},
+        {name: 'vertical-line', id: 'va', label: 'Vertical line', color: 'dimgray', dashStyle: 'dashdotted'},
+        {name: 'line-segment', id: 'ls', label: 'Line segment', color: 'dimgray', dashStyle: 'solid'},
+      ]
+    },
   };
   if (configs.hasOwnProperty(configId)) {
     return configs[configId];
