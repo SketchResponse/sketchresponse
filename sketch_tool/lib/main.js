da0b1fab6d4cc14682703d9b638f4fe7da0985a1
Jean-Michel Claus <jmc@math.mit.edu>
2016-09-20 14:29:00 +0200

    Add keyboard shortcuts for 'Delete' and 'Unselect all'.

--- sketch_tool/lib/main.js
+++ sketch_tool/lib/main.js
@@ -212,6 +212,8 @@ export default class SketchInput {
 
     KeyMaster('⌘+z, ctrl+z', event => { this.messageBus.emit('undo'); return false; });
     KeyMaster('⌘+y, ctrl+y, ⌘+shift+z, ctrl+shift+z', event => { this.messageBus.emit('redo'); return false; });
+    KeyMaster('esc', event => { this.messageBus.emit('deselectAll'); return false; });
+    KeyMaster('delete, backspace', event => { this.messageBus.emit('deleteSelected'); return false; });
     document.addEventListener('mouseenter', event => window.focus());  // So we get keyboard events. Rethink this?
 
     // Allow multitouch zoom on SVG element (TODO: move elsewhere?)
