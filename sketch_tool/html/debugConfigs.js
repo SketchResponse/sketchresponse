ce76a5935a932128e3efbaaaebe72282c81558c1
Jean-Michel Claus <jmc@math.mit.edu>
2016-10-12 12:09:41 +0200

    Include only two debug configs, one of them has cartesian axes, the other polar. Both of them use the totality of plugins. Also, use different and softer colors for each plugin type.

--- sketch_tool/html/debugConfigs.js
+++ sketch_tool/html/debugConfigs.js
@@ -8,47 +8,15 @@ function loadConfig(configId) {
       xscale: 'linear',
       yscale: 'linear',
       plugins: [
-        {name: 'axes'},
-        {name: 'freeform', id: 'f', label: 'Function f(x)', color: 'blue'},
-        {name: 'freeform', id: 'g', label: 'Derivative g(x)', color: 'orange'},
-        {name: 'vertical-line', id: 'va', label: 'Vertical asymptote', color: 'gray', dashStyle: 'dashdotted'},
-        {name: 'horizontal-line', id: 'ha', label: 'Horizontal asymptote', color: 'gray', dashStyle: 'dashdotted'},
-        {name: 'point', id: 'cp', label: 'Critical point', color: 'black', size: 15},
-      ]
-    },
-    config2: {
-      width: 750,
-      height: 420,
-      xrange: [-4.5, 4.5],
-      yrange: [-2.5, 2.5],
-      xscale: 'linear',
-      yscale: 'linear',
-      plugins: [
-        {name: 'axes'},
-        {name: 'freeform', id: 'f', label: 'Function f(x)', color: 'blue'},
-        {name: 'freeform', id: 'g', label: 'Derivative g(x)', color: 'orange'},
-      ]
-    },
-    config3: {
-      width: 750,
-      height: 420,
-      xrange: [-4.5, 4.5],
-      yrange: [-2.5, 2.5],
-      rrange: 2,
-      xscale: 'linear',
-      yscale: 'linear',
-      plugins: [
         {
           name: 'axes',
-          type: 'polar',
-          rmajor: 0.5,
-          thetamajor: 30
+          type: 'cartesian'
         },
         {
           name: 'point',
           id: 'cp',
           label: 'Point',
-          color: 'red',
+          color: 'sienna',
           size: 15
         },
         {
@@ -69,14 +37,14 @@ function loadConfig(configId) {
           name: 'line-segment',
           id: 'ls',
           label: 'Segment',
-          color: 'dimgray',
+          color: 'cornflowerblue',
           dashStyle: 'solid'
         },
         {
           name: 'line-segment',
           id: 'ar',
           label: 'Arrow',
-          color: 'green',
+          color: 'mediumseagreen',
           dashStyle: 'solid',
           arrowHead: {
             length: 10,
@@ -87,7 +55,81 @@ function loadConfig(configId) {
           name: 'line-segment',
           id: 'car',
           label: 'Constrained arrow',
+          color: 'mediumseagreen',
+          dashStyle: 'solid',
+          lengthConstraint: 100,
+          arrowHead: {
+            length: 10,
+            base: 7
+          },
+        },
+        {
+          name: 'freeform',
+          id: 'f',
+          label: 'Function f(x)',
+          color: 'orange'
+        }
+      ]
+    },
+    config2: {
+      width: 750,
+      height: 420,
+      xrange: [-4.5, 4.5],
+      yrange: [-2.5, 2.5],
+      rrange: 2,
+      xscale: 'linear',
+      yscale: 'linear',
+      plugins: [
+        {
+          name: 'axes',
+          type: 'polar',
+          rmajor: 0.5,
+          thetamajor: 30
+        },
+        {
+          name: 'point',
+          id: 'cp',
+          label: 'Point',
+          color: 'sienna',
+          size: 15
+        },
+        {
+          name: 'horizontal-line',
+          id: 'ha',
+          label: 'Horizontal line',
           color: 'dimgray',
+          dashStyle: 'dashdotted'
+        },
+        {
+          name: 'vertical-line',
+          id: 'va',
+          label: 'Vertical line',
+          color: 'dimgray',
+          dashStyle: 'dashdotted'
+        },
+        {
+          name: 'line-segment',
+          id: 'ls',
+          label: 'Segment',
+          color: 'cornflowerblue',
+          dashStyle: 'solid'
+        },
+        {
+          name: 'line-segment',
+          id: 'ar',
+          label: 'Arrow',
+          color: 'mediumseagreen',
+          dashStyle: 'solid',
+          arrowHead: {
+            length: 10,
+            base: 7
+          }
+        },
+        {
+          name: 'line-segment',
+          id: 'car',
+          label: 'Constrained arrow',
+          color: 'mediumseagreen',
           dashStyle: 'solid',
           lengthConstraint: 100,
           arrowHead: {
