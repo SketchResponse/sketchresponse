15713f021ad522db72b9d8cf7022f965a9ef5da8
Jennifer French <jfrench@mit.edu>
2017-03-13 16:33:03 -0400

    phase lines vertical

--- grader_scripts/u2s2_phaseline1_vertical.py
+++ grader_scripts/u2s2_phaseline1_vertical.py
@@ -2,18 +2,18 @@ import sketchresponse
 from grader_lib import GradeableFunction, LineSegment
 
 problemconfig = sketchresponse.config({
-    'width': 750,
-    'height': 150,
-    'xrange': [-3.5, 3.5],
-    'yrange': [-1, 1],
+    'width': 500,
+    'height': 420,
+    'xrange': [-1, 1],
+    'yrange': [-3.5, 3.5],
     'xscale': 'linear',
     'yscale': 'linear',
     'plugins': [
-        {'name': 'axes', 'ylabels': []},
+        {'name': 'axes', 'xmajor':1.5},
         {'name': 'point', 'id': 'stb', 'label': 'Stable', 'color': 'Blue', 'size': 15},
         {'name': 'point', 'id': 'semi', 'label': 'Semistable', 'color': 'Black', 'size': 15},
         {'name': 'point', 'id': 'uns', 'label': 'Unstable', 'color':'Orange','size': 15},
-        {'name': 'line-segment', 'id': 'carw', 'label': 'Arrow', 'color': 'Gray', 'size': 15, 'directionConstraint': 'horizontal', 'lengthContraint': 5, 'arrowHead': {'length': 5, 'base': 10}, 'constrained': True}
+        {'name': 'line-segment', 'id': 'carw', 'label': 'Arrow', 'color': 'Gray', 'size': 15, 'directionConstraint': 'vertical', 'lengthContraint': 5, 'arrowHead': {'length': 5, 'base': 10}, 'constrained': True}
     ]
 })
 
@@ -25,16 +25,16 @@ def grader(stb,semi,uns,carw):
     uns = GradeableFunction.GradeableFunction(uns)
     carw = LineSegment.LineSegments(carw)
 
-    if not stb.has_point_at(x=-2, y= 0):
+    if not stb.has_point_at(x=0, y= -2):
         return False, '<font color="blue"> The location of your stable critical point is incorrect. </font><br />'
 
     if not uns.has_point_at(x=0, y=0):
        return False, '<font color="blue"> The location of your unstable critical point is incorrect. </font><br />'
 
     
-    downarrow = carw.get_segments_at(x=-1)
-    bottomuparrow = carw.get_segments_at(x=-3)
-    topuparrow = carw.get_segments_at(x=2)
+    downarrow = carw.get_segments_at(y=-1)
+    bottomuparrow = carw.get_segments_at(y=-3)
+    topuparrow = carw.get_segments_at(y=2)
 
     if downarrow[0].start.x - downarrow[0].end.x >0:
        return False, '<font color="blue"> The direction of the arrow between your two critical points is not correct. </font><br />'  
