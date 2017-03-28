61cef69037504daa9a41ad31e2c711f6f0578849
bfrench <bfrench@andrew.cmu.edu>
2017-03-28 15:04:48 -0400

    implemented first pass at line segment intersection function. it returns a list of lists of points of intersection between the input segment given as 4 values and each segment that composes the boundary of the polygon.

--- grader_lib/Polygon.py
+++ grader_lib/Polygon.py
@@ -1,7 +1,7 @@
 import Gradeable
 from copy import deepcopy
-from Point import Point
-from sympy import Polygon, Point as symPoint
+from Point import Point as srPoint
+from sympy.geometry import Polygon, Point, Segment, intersection
 
 
 class Polygons(Gradeable.Gradeable):
@@ -23,8 +23,8 @@ class Polygons(Gradeable.Gradeable):
         pointList = []
 
         for px_x, px_y in points:
-            point = Point(self, px_x, px_y)
-            pointList.append([point.x, point.y])
+            point = srPoint(self, px_x, px_y)
+            pointList.append((point.x, point.y))
         
         return pointList
 
@@ -41,8 +41,27 @@ class Polygons(Gradeable.Gradeable):
             
         for p in polygons:
             poly = Polygon(*p) # sympy polygon does not take a list of points, stupidly
-            point = symPoint(x, y)
-
-            contains = contains or poly.encloses_point(point)
+            contains = contains or poly.encloses_point((x, y))
 
         return contains
+
+    def intersectionsWithBoundary(self, x1, y1, x2, y2):
+        intersections = []
+        in_seg = Segment((x1, y1), (x2, y2))
+
+        for polygon in self.polygons:
+            p_intersections = []
+            for i, pt in enumerate(polygon):
+                if i < len(polygon) - 1:
+                    pt2 = polygon[i + 1]
+                else:
+                    pt2 = polygon[0]
+                    
+                poly_seg = Segment(pt, pt2)
+                intersection_points = intersection(in_seg, poly_seg)
+                for ip in intersection_points:
+                    p_intersections.append([ip.x, ip.y])
+
+            intersections.append(p_intersections)
+
+        return intersections
