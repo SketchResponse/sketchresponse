25d7d2dabef44e03e43b647478438246c737346d
bfrench <bfrench@andrew.cmu.edu>
2017-05-04 18:06:49 -0400

    added grader functions for testing point containment and linesegment intersection given a polygon. added grader function to test whether a polygon contains given polygon by testing point containment for each point of the input polygon. added a grader function to test whether a point falls on the boundary of a polygon using the distance function with default distance tolerance of 10 pixels seems to work.

--- grader_lib/Polygon.py
+++ grader_lib/Polygon.py
@@ -10,7 +10,7 @@ class Polygons(Gradeable.Gradeable):
     """
     def __init__(self, info, tolerance=dict()):
         Gradeable.Gradeable.__init__(self, info, tolerance)
-
+        self.set_default_tolerance('point_distance', 10) # threshold for finding a point close to an x value
         self.polygons = []
 
         for spline in info:
@@ -33,22 +33,24 @@ class Polygons(Gradeable.Gradeable):
         """Returns the number of polylines defined in the function."""
         return len(self.polygons)
 
-    def containsPoint(self, x, y, polygon=None):
-        polygons = self.polygons
-        if not polygon is None:
-            polygons = [polygon]
-
+    def containsPoint(self, point):
         contains = False
-            
+
         for p in polygons:
-            poly = Polygon(*p) # sympy polygon does not take a list of points, stupidly
-            contains = contains or poly.encloses_point((x, y))
+            # sympy polygon does not take a list of points, stupidly
+            poly = Polygon(*p)
+            contains = contains or poly.encloses_point(Point(*point))
 
         return contains
 
-    def intersectionsWithBoundary(self, x1, y1, x2, y2):
+    def polygonContainsPoint(self, polygon, point):
+        # sympy polygon does not take a list of points, stupidly
+        poly = Polygon(*polygon)
+        return poly.encloses_point(Point(*point))
+
+    def intersectionsWithBoundary(self, point1, point2):
         intersections = []
-        in_seg = Segment((x1, y1), (x2, y2))
+        in_seg = Segment(Point(*point1), Point(*point2))
 
         for polygon in self.polygons:
             p_intersections = []
@@ -66,3 +68,50 @@ class Polygons(Gradeable.Gradeable):
             intersections.append(p_intersections)
 
         return intersections
+
+    def intersectionsWithPolygonBoundary(self, point1, point2, polygon):
+        intersections = []
+        in_seg = Segment(Point(*point1), Point(*point2))
+
+        for i, pt in enumerate(polygon):
+            if i < len(polygon) - 1:
+                pt2 = polygon[i + 1]
+            else:
+                pt2 = polygon[0]
+
+            poly_seg = Segment(pt, pt2)
+            intersection_points = intersection(in_seg, poly_seg)
+            for ip in intersection_points:
+                intersections.append([ip.x, ip.y])
+
+        return intersections
+
+    def containsPolygon(self, polygon):
+        for p in self.polygons:
+            contains = True
+            for point in polygon:
+                contains = contains and self.polygonContainsPoint(p, point)
+
+            if contains:
+                return p
+
+        return None
+
+    def pointOnBoundary(self, point, distTolerance=None):
+        if distTolerance is None:
+            distTolerance = self.tolerance['point_distance'] / self.xscale
+        
+        for polygon in self.polygons:
+            for i, pt in enumerate(polygon):
+                if i < len(polygon) - 1:
+                    pt2 = polygon[i + 1]
+                else:
+                    pt2 = polygon[0]
+
+                poly_seg = Segment(pt, pt2)
+                distance = poly_seg.distance(Point(*point))
+                if distance < distTolerance:
+                    print distance
+                    return polygon
+
+        return None
