9cff55493306467b89c7c157e5781f2eefa6b21a
Jennifer French <jfrench@mit.edu>
2017-05-15 12:28:01 -0400

    script and smaller icon

--- grader_scripts/ps2_polezero_mathlet.py
+++ grader_scripts/ps2_polezero_mathlet.py
@@ -29,12 +29,29 @@ def grader(p,z):
 
     if not z.has_point_at(x=0,y=2) or not z.has_point_at(x=0,y=-2) or not z.has_point_at(x=0,y=3) or not z.has_point_at(x=0,y=-3):
         return False, '<font color="blue">Check the location of your zeros. Can you use the mathlet to find a better location?<br/></font>'
-    # try:
-    #     p1 =  p.get_closest_point_to(x=0,y=2)
-    #     p2 =  p.get_closest_point_to(x=0,y=-2)
-    #     p3 =  p.get_closest_point_to(x=0,y=3)
-    #     p4 =  p.get_closest_point_to(x=0,y=-3)
-    
 
+    try:
+        z1 = z.get_point_at(y=2)
+        z2 = z.get_point_at(y=-2)
+        z3 = z.get_point_at(y=3)
+        z4 = z.get_point_at(y=-2)
+
+        # p1 =  p.closest_point_to_point(z1)
+        # p2 =  p.closest_point_to_point(z2)
+        # p3 =  p.closest_point_to_point(z3)
+        # p4 =  p.closest_point_to_point(z4)
+
+        p1 =  p.get_point_at(y=2,squareDistTolerance=10000)
+        p2 =  p.get_point_at(y=-2,squareDistTolerance=10000)
+        p3 =  p.get_point_at(y=3,squareDistTolerance=10000)
+        p4 =  p.get_point_at(y=-3,squareDistTolerance=10000)
+
+        if p1.x > 0 or p2.x > 0 or p3.x > 0 or p4.x > 0:
+            return False, '<font color="blue">Your system is unstable. Can you find a stable system with the desired properties?<br/></font>'
+
+        if p1.y > 2 or p2.y < -2 or p3.y > 3 or p4.y < -3:
+            return False, '<font color="blue">Check the locations of your poles. Something seems off.<br/></font>'
+    except:
+        return False, '%s, %s, %s, %s; %s, %s, %s, %s' %(z1,z2,z3,z4,p.get_point_at(y=2,squareDistTolerance=10000),p.get_point_at(y=-2,squareDistTolerance=10000),p.get_point_at(y=3,squareDistTolerance=10000),p.get_point_at(y=-3,squareDistTolerance=10000))
 
     return True, 'Good Job'
