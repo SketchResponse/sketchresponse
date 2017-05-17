b1597a9bc2ec7f7bac07969b15a32fe9a3ba2a06
bfrench <bfrench@andrew.cmu.edu>
2017-05-17 15:31:29 -0400

    added api doc comments to the polygon grader. added a couple extra grader functions so all of them provide either a check against all polygons in the grader and a function to check just a given polygon.

--- grader_lib/Polygon.py
+++ grader_lib/Polygon.py
@@ -31,11 +31,21 @@ class Polygons(Gradeable.Gradeable):
         return pointList
 
     def get_polygon_count(self):
-        """Returns the number of polylines defined in the function."""
+        """Returns the number of polygons defined in the function."""
         return len(self.polygons)
 
     def contains_point(self, point, tolerance=None):
-        contains = False
+        """Return the polygon that contains the given point, within tolerance.
+
+        Args:
+            point: an list [x, y] defining a point, or
+                   a Point object from a GradeableFunction grader
+            tolerance: a pixel distance tolerance
+        Returns:
+            list:
+            The first polygon, defined as a list of points, that contains
+            the given point, or None.
+        """
 
         if isinstance(point, SR_Point):
             point = [point.x, point.y]
@@ -45,11 +55,25 @@ class Polygons(Gradeable.Gradeable):
             poly = Polygon(*p)
             isInside = poly.encloses_point(Point(*point))
             onBoundary = self.pointOnBoundary(point, tolerance=tolerance)
-            contains = contains or isInside or onBoundary
+            if isInside or onBoundary:
+                return p
 
-        return contains
+        return None
 
     def polygon_contains_point(self, polygon, point, tolerance=None):
+        """Return whether the given point is contained within the given
+           polygon, within tolerance.
+
+        Args:
+            polygon: a list of points [[x1,y1], ..., [xn,yn]] defining a polygon
+            point: an list [x, y] defining a point, or
+                   a Point object from a GradeableFunction grader
+            tolerance: a pixel distance tolerance
+        Returns:
+            boolean:
+            True if the point is contained within the polygon within tolerance,
+            otherwise False.
+        """
         # sympy polygon does not take a list of points, stupidly
         if isinstance(point, SR_Point):
             point = [point.x, point.y]
@@ -60,7 +84,127 @@ class Polygons(Gradeable.Gradeable):
 
         return isInside or onBoundary
 
+    def contains_polygon(self, polygon, tolerance=None):
+        """Return the polygon that contains the given polygon, within tolerance.
+
+        Args:
+            polygon: a list of points [[x1,y1], ..., [xn,yn]] defining a polygon
+            tolerance: a pixel distance tolerance
+        Returns:
+            list:
+            The first polygon, defined as a list of points, that contains
+            the given polygon, or None.
+        """
+        for p in self.polygons:
+            contains = True
+            for point in polygon:
+                contains = contains and self.polygonContainsPoint(p, point,
+                                                                  tolerance=tolerance)
+
+            if contains:
+                return p
+
+        return None
+
+    def polygon_contains_polygon(self, container, contained, tolerance=None):
+        """Return whether the container polygon contains the entirety of the 
+           contained polygon, within tolerance.
+
+        Args:
+            container: a list of points [[x1,y1], ..., [xn,yn]] defining a polygon
+            contained: a list of points [[x1,y1], ..., [xn,yn]] defining a polygon
+            tolerance: a pixel distance tolerance
+        Returns:
+            list:
+            True of the container polygon contains every point of the contained
+            polygon, otherwise False.
+        """
+        contains = True
+        for point in contained:
+            contains = contains and self.polygonContainsPoint(container, point,
+                                                              tolerance=tolerance)
+
+        return contains
+
+    def point_is_on_boundary(self, point, tolerance=None):
+        """Return the polygon on whose boundary the given point lies,
+           within tolerance.
+
+        Args:
+            point: an list [x, y] defining a point, or
+                   a Point object from a GradeableFunction grader
+            tolerance: a pixel distance tolerance
+        Returns:
+            list:
+            The first polygon, defined as a list of points, that contains
+            the given point, or None.
+        """
+        if tolerance is None:
+            tolerance = self.tolerance['point_distance'] / self.xscale
+
+        if isinstance(point, SR_Point):
+            point = [point.x, point.y]
+
+        for polygon in self.polygons:
+            for i, pt in enumerate(polygon):
+                if i < len(polygon) - 1:
+                    pt2 = polygon[i + 1]
+                else:
+                    pt2 = polygon[0]
+
+                poly_seg = Segment(pt, pt2)
+                distance = poly_seg.distance(Point(*point))
+                if distance < tolerance:
+                    return polygon
+
+        return None
+
+    def point_is_on_polygon_boundary(self, polygon, point, tolerance=None):
+        """Return whether the given point lies on the boundary of the given
+           polygon, within tolerance.
+
+        Args:
+            polygon: a list of points [[x1,y1], ..., [xn,yn]] defining a polygon
+            point: an list [x, y] defining a point, or
+                   a Point object from a GradeableFunction grader
+            tolerance: a pixel distance tolerance
+        Returns:
+            boolean:
+            True if the point lies on the polygon boundary within tolerance,
+            otherwise False.
+        """
+        if tolerance is None:
+            tolerance = self.tolerance['point_distance'] / self.xscale
+
+        if isinstance(point, SR_Point):
+            point = [point.x, point.y]
+
+        for i, pt in enumerate(polygon):
+            if i < len(polygon) - 1:
+                pt2 = polygon[i + 1]
+            else:
+                pt2 = polygon[0]
+
+            poly_seg = Segment(pt, pt2)
+            distance = poly_seg.distance(Point(*point))
+            if distance < tolerance:
+                return True
+
+        return False
+
     def get_intersections_with_boundary(self, line_segment, tolerance=None):
+        """Return a list of lists of intersection points of the given
+            line segment with the grader polygons.
+
+        Args:
+            line_segment: an list of two points [[x1, y1], [x2,y2]], or
+                          a LineSegment object from a LineSegment grader
+            tolerance: a pixel distance tolerance
+        Returns:
+            list:
+            A list of lists of intersection points [x,y] for each polygon
+            in the grader.
+        """
         intersections = []
         if isinstance(line_segment, LineSegment):
             point1 = line_segment.getStartPoint()
@@ -93,6 +237,18 @@ class Polygons(Gradeable.Gradeable):
 
     def get_intersections_with_polygon_boundary(self, line_segment,
                                             polygon, tolerance=None):
+        """Return a list of intersection points of the given line segment
+           with the given polygon.
+
+        Args:
+            line_segment: an list of two points [[x1, y1], [x2,y2]], or
+                          a LineSegment object from a LineSegment grader
+            polygon: a list of points [[x1,y1], ..., [xn,yn]] defining a polygon
+            tolerance: a pixel distance tolerance
+        Returns:
+            list:
+            A list of intersection points [x,y].
+        """
         intersections = []
         if isinstance(line_segment, LineSegment):
             point1 = line_segment.getStartPoint()
@@ -120,38 +276,6 @@ class Polygons(Gradeable.Gradeable):
 
         return intersections
 
-    def contains_polygon(self, polygon, tolerance=None):
-        for p in self.polygons:
-            contains = True
-            for point in polygon:
-                contains = contains and self.polygonContainsPoint(p, point,
-                                                                  tolerance=tolerance)
-
-            if contains:
-                return p
-
-        return None
-
-    def point_is_on_boundary(self, point, tolerance=None):
-        if tolerance is None:
-            tolerance = self.tolerance['point_distance'] / self.xscale
-
-        if isinstance(point, SR_Point):
-            point = [point.x, point.y]
-
-        for polygon in self.polygons:
-            for i, pt in enumerate(polygon):
-                if i < len(polygon) - 1:
-                    pt2 = polygon[i + 1]
-                else:
-                    pt2 = polygon[0]
-
-                poly_seg = Segment(pt, pt2)
-                distance = poly_seg.distance(Point(*point))
-                if distance < tolerance:
-                    return polygon
-
-        return None
 
     ###################
     # Helper Functions
