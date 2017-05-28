4a31da89162b486d40f5f398dc09c3a938ab90d2
bfrench <bfrench@andrew.cmu.edu>
2017-05-28 11:49:19 -0400

    added api doc comments to the api functions of the Tag class

--- grader_lib/Tag.py
+++ grader_lib/Tag.py
@@ -7,9 +7,25 @@ class Tag(object):
         self.tag = tag
 
     def get_tag(self):
+        """Get the value of the tag.
+
+        Returns:
+            string:
+            The tag string of this object.
+        """
         return self.tag
 
     def tag_equals(self, to_compare):
+        """Returns whether the given string is equal to the object's tag value.
+
+        Args:
+            to_compare: an string value
+
+        Returns:
+            bool:
+            True if the given string is the same as the object's tag,
+            otherwise False.
+        """
         return self.tag == to_compare
 
 
@@ -22,6 +38,16 @@ class Tagables(object):
         self.tags = tags
 
     def contains_tag(self, to_compare):
+        """Return a reference to the first object found with the given tag value.
+
+        Args:
+            to_compare: an string value
+
+        Returns:
+            Tag:
+            The first Tag object with a tag value of to_compare, if there
+            are no matching tags, returns None.
+        """
         if not self.tags is None:
             for tag in self.tags:
                 if tag.tag_equals(to_compare):
