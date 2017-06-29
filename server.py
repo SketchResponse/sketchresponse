0bd9ea5f9ef062867a6ac8796e1b2e83ef795300
Jean-Michel Claus <jmc@math.mit.edu>
2017-06-29 22:07:15 +0200

    Add default route and grader.

--- server.py
+++ server.py
@@ -5,27 +5,10 @@ from proto2prod import convert_ans_dict
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
-
-@app.route('/check', methods=['POST'])
-def check():
-    submitted_data = request.get_json()
-    submitted_data = convert_ans_dict(submitted_data)  # Fake the production API
-
-    # Dispatch to grader in the required format
-    # Assume no 'expect' value for now
-    grader_response = gradefn(None, json.dumps(submitted_data))
-
-    # Allow boolean grader return values instead of dicts
-    if type(grader_response) == bool:
-        grader_response = {'ok': grader_response, 'msg': ''}
-
-    return json.dumps(grader_response)
-
-
 @app.route('/<grader_module_name>')
-def new_local_frontend(grader_module_name):
+def new_local_frontend(grader_module_name=None):
+    if grader_module_name is None:
+        grader_module_name = 'default_grader'
     grader_module = importlib.import_module('grader_scripts.' + grader_module_name)
     reload(grader_module)
 
