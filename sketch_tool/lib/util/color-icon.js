8fba8adf446c8ff4ae5c0beff4eee3a44fc74d74
Jean-Michel Claus <jmc@math.mit.edu>
2017-07-26 10:01:08 +0200

    Add missing new file.

---
+++ sketch_tool/lib/util/color-icon.js
@@ -0,0 +1,21 @@
+// Colors an SVG icon and returns a blob url
+export default function colorIcon(src, stroke, fill) {
+  let ajax = new XMLHttpRequest(), div, svg, svgData, blob;
+  ajax.open('GET', src, false); // For the time being, use a deprecated synchronous request
+  ajax.send(null);
+  div = document.createElement('div');
+  div.innerHTML = ajax.responseText;
+  svg = div.children[0];
+  svg.setAttribute('width', 35);
+  svg.setAttribute('height', 35);
+  svg.setAttribute('stroke', stroke);
+  svg.setAttribute('fill', fill);
+  // Convert colored svg to an image
+  // http://www.timvasil.com/blog14/post/2014/02/06/How-to-convert-an-SVG-image-into-a-static-image-with-only-JavaScript.aspx
+  // Without the charset part or it will fail in Safari
+  // http://stackoverflow.com/questions/23114686/safari-image-onload-event-not-firing-with-blob-url
+  svgData = new XMLSerializer().serializeToString(svg);
+  blob = new Blob([svgData], { type: "image/svg+xml" });
+  // Return the blob's URL
+  return (self.URL || self.webkitURL || self).createObjectURL(blob);
+}
