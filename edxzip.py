de83a1993ccd32f7fb3a288722a6385f34c3d82b
Martin Segado <msegado@mit.edu>
2020-12-03 21:35:18 -0500

    Fix edxzip.py to create a package within python_lib.zip

--- edxzip.py
+++ edxzip.py
@@ -1,15 +1,26 @@
 import zipfile
 import os
+import re
+
+
+def addfile(source, ziph):
+    target = os.path.join('sketchresponse/', source)
+    print('Adding ' + target)
+    ziph.write(source, target)
 
 
 def zipdir(path, ziph):
     # ziph is zipfile handle
     for root, dirs, files in os.walk(path):
         for file in files:
-            ziph.write(os.path.join(root, file))
+            if re.fullmatch(r'.+\.py|LICENSE', file):
+                addfile(os.path.join(root, file), ziph)
+
 
 if __name__ == '__main__':
     zipf = zipfile.ZipFile('python_lib.zip', 'w', zipfile.ZIP_DEFLATED)
-    zipf.write('sketchresponse.py')
+    addfile('__init__.py', zipf)
+    addfile('sketchresponse.py', zipf)
+    addfile('LICENSE', zipf)
     zipdir('grader_lib/', zipf)
     zipf.close()
